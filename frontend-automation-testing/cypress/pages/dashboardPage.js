8fd7a06dc3dc3f740297f450f12e6cd958e4181e
Charbel Atallah <Chappa_atallah@hotmail.com>
2021-11-03 21:46:13 +0100

    cleaned old codes from dashboard

--- frontend-automation-testing/cypress/pages/dashboardPage.js
+++ frontend-automation-testing/cypress/pages/dashboardPage.js
@@ -2,17 +2,9 @@
 
 //Element
 const titleOfDash= 'Testers Hotel'
-//const titleOfRooms='Floor'
 const titleOfBackBtn='Back'
-//const titleOfClients='#1'
-//const titleOfBills='ID:'
-//const titleOfReservations='Reservations'
-
-//const viewRooms=':nth-child(1) > .btn' //Rooms btn
 const viewDashboard=':nth-child(3) > .btn' //Back btn
-//const viewClient='.blocks > :nth-child(2) > .btn' //Client btn
-//const viewBill=':nth-child(3) > .btn' //bill btn
-//const viewReservations=':nth-child(4) > .btn' //Reservation btn
+
 
 //Actions
 function checkTitleOfdashboardPage(cy){
@@ -21,9 +13,6 @@ function checkTitleOfdashboardPage(cy){
 
 function clickOnDashboardPages(cy){
     cy.get(viewDashboard).click()
-    //cy.get(viewClient).click()
-   // cy.get(viewBill).click()
-   // cy.get(viewReservations).click()
 }
 //Exports
 module.exports = {
